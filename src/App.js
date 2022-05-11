a96f752a0f2b97d3cf8392984818da97317f1ed7
Jfontaine <jfontaine19@gmail.com>
2022-05-11 18:20:37 -0400

    Added full list of categories

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
 import Row from "./Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="app">
       <h1>Notflix!</h1>
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" />
+      <Row
+        title="NOTFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }

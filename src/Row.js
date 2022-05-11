2b9f66ebd7eb4f8d8674737bb374abe37dadc1bb
Jfontaine <jfontaine19@gmail.com>
2022-05-11 18:44:39 -0400

    added hidden scroll bar and hover styling

--- src/Row.js
+++ src/Row.js
@@ -28,6 +28,7 @@ function Row({ title, fetchUrl }) {
 
         {movies.map((movie) => (
           <img
+            key={movie.id}
             className="row__poster"
             src={`${base_url}${movie.poster_path}`}
             alt={movie.name}
@@ -39,4 +40,3 @@ function Row({ title, fetchUrl }) {
 }
 
 export default Row;
-

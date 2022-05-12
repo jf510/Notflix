e0e949b79a88c32b9875544492a531f8f8470911
Jfontaine <jfontaine19@gmail.com>
2022-05-11 22:19:42 -0400

    created state for movies. Added useEffect function to fetch data.

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import requests from "./requests";
 
 function Banner() {
-  return <header className="banner"></header>;
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * requests.data.results.length - 1)
+        ]
+      );
+      //
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <header className="banner">
+      {/** Background image */}
+      {/** title */}
+      {/** div > 2 buttons */}
+      {/** description */}
+    </header>
+  );
 }
 
 export default Banner;
